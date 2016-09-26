b042db1ecfe12876550fe5ba6811abf7118407a0
Alexander Korotin <alacore@gmail.com>
2016-09-26 23:55:14 +0300

    Refactop API with Underscore.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -32,14 +33,7 @@ app.get('/todos', function(req, res) {
 
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id,10);
-    var matchedTodo;
-
-    todos.forEach(function(todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
-
+    var matchedTodo = _.findWhere(todos,{id:todoId});
     if (matchedTodo) {
         res.json(matchedTodo);
     } else {
@@ -48,8 +42,15 @@ app.get('/todos/:id', function(req, res) {
     // res.send('Asking for todo with id of ' + req.params.id);
 });
 
-app.post('/todos/',function(req,res){
-	var body = req.body;
+app.post('/todos', function(req,res){
+	var body = _.pick(req.body, 'description', 'completed');
+	
+	if (_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
 	body.id = todoNextId++;
 	todos.push(body);
 
